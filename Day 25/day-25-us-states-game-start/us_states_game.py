bf21cf7289727195cc83432867a07e9757a27dcc
ryaseen <ryaseen.bscs22seecs@seecs.edu.pk>
2025-12-05 19:38:09 +0500

    states guessing game

---
+++ Day 25/day-25-us-states-game-start/us_states_game.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import turtle
+
+
+screen = turtle.Screen()
+screen.title("U.S States Game")
+screen.setup(650,650)
+states_data = pd.read_csv("Day 25/day-25-us-states-game-start/50_states.csv")
+
+# print(states_positions.head())
+
+image ="Day 25/day-25-us-states-game-start/blank_states_img.gif"
+screen.addshape(image)
+
+turtle.shape(image)
+
+# get position of mouse click(x,y) coordinates
+# def get_mouse_click_coor(x,y):
+#     print(x,y)
+    
+# turtle.onscreenclick(get_mouse_click_coor)
+
+# input_turtle = Turtle()
+# input_turtle.hideturtle()
+# input_turtle.penup()
+# input_turtle.goto(x=-620,y=620)
+
+correct_guess = []
+
+while len(correct_guess) < 50:
+    # turtle.penup()
+    # turtle.setx(-620)
+    # turtle.sety(620)
+    # screen.goto(x=-620,y=620)
+    user_answer = screen.textinput(title=f"{len(correct_guess)}/50 Guess the State",prompt="What's another state's name?").title()
+
+
+
+    
+    # print(user_answer)
+    states_names = list(states_data["state"])
+    # print(list(states_names))
+    if user_answer in states_names:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        data = states_data[states_data["state"] == user_answer]
+        correct_guess.append(user_answer)
+        x_cor = list(data['x'])[0]
+        y_cor = list(data['y'])[0]
+        
+        print(x_cor,y_cor)
+        t.goto(x=x_cor,y=y_cor)
+        t.write(f"{user_answer}")
+
+
+
+
+
+
+
+
--- Day 25/squirrel.py
+++ Day 25/squirrel.py
@@ -5,13 +5,23 @@ import numpy as np
 
 df = pd.read_csv("Day 25/2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
 
-fur_colors_dict = pd.DataFrame(df["Primary Fur Color"].value_counts())
-fur_colors_dict.to_csv('squirrel_count.csv')
+# fur_colors_dict = pd.DataFrame(df["Primary Fur Color"].value_counts())
+# fur_colors_dict.to_csv('squirrel_count.csv')
 # print(fur_colors_dict)
 
+gray_sq = df[df["Primary Fur Color"] == "Gray"]
+cinnamom_sq = df[df["Primary Fur Color"] == "Cinnamon"]
+black_sq = df[df["Primary Fur Color"] == "Black"]
 
-
-
+gray_sq_count = len(gray_sq)
+cinnamom_sq_count = len(cinnamom_sq)
+black_sq_count = len(black_sq)
+df_dict = {
+    "Fur Color": ["Gray","Cinnamon","Black"],
+    "Count": [gray_sq_count,cinnamom_sq_count,black_sq_count]
+}
+data_df = pd.DataFrame(df_dict)
+data_df.to_csv("sq_count.csv")
 
 
 
